dc037c65bc61487f456ccabb725404de0205c252
Your Name <your.email@example.com>
2026-03-03 21:50:17 +0530

    Implement product retrieval endpoints

---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+const Product = require("../models/Product");
+
+// GET /products
+exports.getProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// GET /products/:id
+exports.getProductById = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    next(error);
+  }
+};
